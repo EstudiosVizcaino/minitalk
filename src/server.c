c413c41ff2fce4bbab2e15638fa0df89c1741d8a
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-07-15 21:47:59 +0200

    Refactored server

--- src/server.c
+++ src/server.c
@@ -6,62 +6,90 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 17:54:40 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/07/15 12:47:22 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/07/15 21:21:20 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-static volatile pid_t	g_client_pid = 0;
+// Justified global: holds the client's PID across multiple signal handler
+// calls, which is necessary to form a complete message.
+static volatile t_server_state	g_state;
 
-static void	handle_completed_byte(unsigned char *byte, int *bit_index)
+/**
+ * @brief Resets the server's state to be ready for a new message.
+ */
+void	reset_state(void)
 {
-	if (*byte == '\0')
-	{
-		ft_printf("\n");
-		kill(g_client_pid, SIGUSR1);
-		g_client_pid = 0;
-	}
-	else
-	{
-		ft_printf("%c", *byte);
-	}
-	*byte = 0;
-	*bit_index = 0;
+	g_state.current_char = 0;
+	g_state.bit_index = 0;
+	g_state.client_pid = 0;
 }
 
+/**
+ * @brief Processes a fully received byte and resets the state for the next.
+ *
+ * This function is called once all 8 bits of a character have been
+ * received. It prints the character and then clears the bit index and
+ * character buffer to prepare for the next incoming byte.
+ */
+static void	handle_completed_byte(void)
+{
+	ft_printf("%c", g_state.current_char);
+	g_state.bit_index = 0;
+	g_state.current_char = 0;
+}
+
+/**
+ * @brief Main signal handler for the mandatory server.
+ * @param sig The signal received.
+ * @param info Struct containing the sender's PID.
+ * @param ucontext Unused.
+ *
+ * Reconstructs bytes bit-by-bit from incoming signals. It locks onto a
+ * client's PID for the duration of a message to handle concurrent clients
+ * gracefully. The acknowledgment order is critical to prevent deadlocks.
+ */
 void	sig_handler(int sig, siginfo_t *info, void *ucontext)
 {
-	static unsigned char	current_char = 0;
-	static int				bit_index = 0;
-
 	(void) ucontext;
-	if (g_client_pid == 0)
-		g_client_pid = info->si_pid;
-	else if (info->si_pid != g_client_pid)
-		return ;
-	current_char = (current_char << 1) | (sig == SIGUSR2);
-	bit_index++;
-	if (bit_index == 8)
-		handle_completed_byte(&current_char, &bit_index);
-	if (g_client_pid)
-		kill(g_client_pid, SIGUSR1);
+	if (g_state.client_pid == 0)
+		g_state.client_pid = info->si_pid;
+	else if (g_state.client_pid != info->si_pid)
+		return;
+	g_state.current_char = (g_state.current_char << 1) | (sig == SIGNAL_BIT_1);
+	g_state.bit_index++;
+	if (g_state.bit_index == BITS_IN_BYTE)
+	{
+		if (g_state.current_char == '\0')
+			ft_printf("\n");
+		else
+			handle_completed_byte();
+	}
+	if (g_state.client_pid != 0)
+		kill(g_state.client_pid, SIGNAL_ACK_BIT);
+	if (g_state.current_char == '\0' && g_state.bit_index == BITS_IN_BYTE)
+		reset_state();
 }
 
+/**
+ * @brief Main function for the server program.
+ */
 int	main(void)
 {
 	struct sigaction	sa;
 
-	ft_printf("Server PID: %d\n", getpid());
-	sigemptyset(&sa.sa_mask);
-	sigaddset(&sa.sa_mask, SIGUSR1);
-	sigaddset(&sa.sa_mask, SIGUSR2);
-	sa.sa_sigaction = sig_handler;
+	ft_printf("Server running with PID: %d\n", getpid());
+	reset_state();
 	sa.sa_flags = SA_SIGINFO;
-	if (sigaction(SIGUSR1, &sa, NULL) == -1
-		|| sigaction(SIGUSR2, &sa, NULL) == -1)
+	sa.sa_sigaction = sig_handler;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGNAL_BIT_0);
+	sigaddset(&sa.sa_mask, SIGNAL_BIT_1);
+	if (sigaction(SIGNAL_BIT_0, &sa, NULL) == -1
+		|| sigaction(SIGNAL_BIT_1, &sa, NULL) == -1)
 	{
-		ft_printf("Signal registration error.\n");
+		ft_printf("Error: Failed to register signal handlers.\n");
 		exit(1);
 	}
 	while (1)
