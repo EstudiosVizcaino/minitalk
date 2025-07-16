0b67c7d03d6e83dcc1e7575cb538a3a54c9b5093
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-07-16 17:48:28 +0200

    Removed `ft_atoi()` from client.c

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 01:29:20 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/07/16 16:21:05 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/07/16 16:56:50 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,6 @@
 
 /// Global flag set by the server to acknowledge reception of each bit.
 static volatile sig_atomic_t	g_server_ack = 1;
-
-int	ft_atoi(const char *str)
-{
-	int		sign;
-	long	nbr;
-
-	sign = 1;
-	nbr = 0;
-	while (*str == ' ' || (*str >= 9 && *str <= 13))
-		str++;
-	if (*str == '-')
-		sign = -1;
-	if (*str == '-' || *str == '+')
-		str++;
-	while (*str >= '0' && *str <= '9')
-	{
-		nbr = nbr * 10 + (*str - '0');
-		str++;
-	}
-	return ((int)(nbr * sign));
-}
 
 /**
  * @brief 		Signal Handler for the server's bit-by-bit acknowledgment.
